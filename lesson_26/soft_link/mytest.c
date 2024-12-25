0b802123d719060ca1373807f147b7e8b5aa33e6
Joeyjiang0217 <93761594+Joeyjiang0217@users.noreply.github.com>
2024-12-25 12:11:04 -0800

    sotf link, hard link

---
+++ lesson_26/soft_link/mytest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello soft link\n");
+    printf("hello soft link\n");
+    printf("hello soft link\n");
+    printf("hello soft link\n");
+    printf("hello soft link\n");
+    printf("hello soft link\n");
+
+    return 0;
+}
